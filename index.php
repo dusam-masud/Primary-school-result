34553eafdf1206ed43eb9bfe9d6549e2f76f40db
dusam-masud <saimum.masudrana@gmail.com>
2022-03-11 16:24:13 +0600

    dashboard start

--- index.php
+++ index.php
@@ -5,11 +5,55 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PSR-Primary School Result</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">PSR</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                </ul>
+            <form class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-warning" type="submit">Search</button>
+            </form>
+            </div>
+        </div>
+    </nav>
+
+
+    <!-- Main Container -->
+    <div class="container">
+        <div class="row my-3 border-bottom">
+            <div class="col">
+                <h1>Dashboard</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat suscipit animi dolores incidunt iste ab earum voluptas nobis tempora enim quam, optio aperiam magnam reprehenderit a eius aliquam! Recusandae, suscipit cum. Eveniet odit tempora iusto aliquid corporis blanditiis nemo cumque accusamus voluptatibus et voluptatum dolorem quisquam sit reiciendis officia mollitia quasi voluptate beatae velit consequatur, quos numquam. Nobis dolorem quod voluptatibus quisquam ipsum corrupti aspernatur commodi dicta magni excepturi, eveniet sunt maiores. Perspiciatis iusto officia dignissimos nemo aut pariatur corrupti? Neque quos eligendi quia dicta reprehenderit magni. Adipisci fuga ipsam magnam modi ratione veritatis suscipit? Maxime nam aspernatur dignissimos odit.
+            </div>
+        </div>
+    </div>
     
 
+    <!-- Footer -->
+    <div class="container-fluid bg-dark text-light mt-3">
+        <div class="copy text-center py-3">
+            &copy; by Masud Rana
+        </div>
+    </div>
+ 
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 <!-- JS Link -->
 <script type="text/css" src="assets/main.js"></script>
 </body>
